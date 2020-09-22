5055f382583b78a1b767d75fafca8326493b4cdf
Magicevandude <70821805+Magicevandude@users.noreply.github.com>
2020-09-22 11:54:39 -0400

    Lab2

---
+++ Labs/lab2/js/Cat_Drawing.js
@@ -0,0 +1,21 @@
+
+function setup() {
+    createCanvas(800, 600);
+}
+// drawing a cat
+function draw() {
+    background(204);
+    line(20, 50, 420, 110);
+    triangle(25,20,45,40,70,75);
+    rect(160, 30, 260, 20);
+    line(20, 50, 420, 110);
+    line(20, 50, 420, 110);
+    ellipse(20,40,5,7);
+    ellipse(10,30,2,5);
+    //if (mouseIsPressed) {
+    //fill(0);
+  } //else {
+    //fill(255);
+    
+  
+    //ellipse(mouseX, mouseY, 80, 80);
