fc145a432ee538eb00d00b49233ab8d7bff67bc9
Magicevandude <70821805+Magicevandude@users.noreply.github.com>
2021-09-15 11:37:54 -0400

    updates to lab

--- Labs/lab2/js/animation.js
+++ Labs/lab2/js/animation.js
@@ -10,14 +10,16 @@ function setup() {
 
 function draw() {
     background(62, 237, 108);
-    
-}
     if(mouseX < 200) {
         fill(28, 58, 232);
     } else {
         fill(240, 5, 5);
     }
-    circle(25, 25, 50, 50);
+    circle(mouseX, mouseY, 50, 50);
     
+    
+    
+}
+  
 
 
