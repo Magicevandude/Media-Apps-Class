c4ef1cc6e2c7088c9e1bb9d61f084402a43a76b2
Magicevandude <70821805+Magicevandude@users.noreply.github.com>
2021-10-26 19:06:07 -0400

    update

---
+++ Labs/lab5/js/tipcalc.js
@@ -0,0 +1,10 @@
+
+// variable to calculate tip. Defaulting to 15% as that is regarded as the standard tip
+let x = 1.15;
+
+// function to take user input as bill and do multiplication based on a 15% boost for tip.
+var y = parseFloat(prompt("Please enter your Bill in Dollars & Cents", ""));
+function tipCalc(y, x) {
+    return y * x;
+
+}
